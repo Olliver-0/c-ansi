be20f4a8a54b1cd9836b00c1164437eb4a55400e
Olliver-0 <nata.gabriel.de.oliveira@gmail.com>
2025-06-09 11:18:44 -0300

    Primeiro exercício

---
+++ aula03/exercicio01.cpp
@@ -0,0 +1,16 @@
+/*
+1.	Construa um programa que mostre seus dados pessoais em tela como: nome, 
+endereço, telefone, e-mail, CPF, RG, e outros...
+R.:
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+	printf("Nome: Manfred Heil Junior\n");
+	printf("Endereco: Rua Antonio Jose Waltrick n.172\n");
+	printf("Telefone: 47 991715494\n");
+	printf("E-mail: manfred@manfred.com.br\n");
+	printf("CPF: 000.000.000-00\n");
+	return 0;
+}
