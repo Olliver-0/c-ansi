b6f1c5e48957c39c7407385f5ebab71780d1e33e
Olliver-0 <nata.gabriel.de.oliveira@gmail.com>
2025-06-11 08:28:04 -0300

    Segundo exercício

---
+++ aula03/exercicio02.cpp
@@ -0,0 +1,17 @@
+/*
+2.	Construa um programa que mostre em tela um cartão de visita, 
+use | e – para fazer o cartão.
+R.:
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+	printf("--------------------------\n");
+	printf("|  Manfred Heil Junior   |\n");
+	printf("| manfred@manfred.com.br |\n");
+	printf("|      47 991715494      |\n");
+	printf("--------------------------\n");
+	
+	return 0;
+}
